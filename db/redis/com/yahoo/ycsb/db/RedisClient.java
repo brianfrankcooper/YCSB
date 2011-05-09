dfe7ccbf8ed31d70e53e373b55ada7b61d30de3c
Robert Lehmann <ycsb@robertlehmann.de>
2011-05-09 08:54:00 +0000

    fixed stub implementation of redis backend

--- db/redis/com/yahoo/ycsb/db/RedisClient.java
+++ db/redis/com/yahoo/ycsb/db/RedisClient.java
@@ -7,6 +7,10 @@ package com.yahoo.ycsb.db;
 import com.yahoo.ycsb.DB;
 import com.yahoo.ycsb.DBException;
 
+import java.util.HashMap;
+import java.util.Set;
+import java.util.Vector;
+
 public class RedisClient extends DB {
     public void init() throws DBException {
     }
@@ -14,23 +18,28 @@ public class RedisClient extends DB {
     @Override
     public int read(String table, String key, Set<String> fields,
             HashMap<String, String> result) {
+        return 0;
     }
 
     @Override
     public int insert(String table, String key, HashMap<String, String> values) {
+        return 0;
     }
 
     @Override
     public int delete(String table, String key) {
+        return 0;
     }
 
     @Override
     public int update(String table, String key, HashMap<String, String> values) {
+        return 0;
     }
 
     @Override
     public int scan(String table, String startkey, int recordcount,
             Set<String> fields, Vector<HashMap<String, String>> result) {
+        return 0;
     }
 
 }
