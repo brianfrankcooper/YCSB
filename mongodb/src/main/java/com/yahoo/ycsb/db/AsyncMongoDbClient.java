61b96944a3818ecba2b918360434cc395d4292e3
Robert J. Moore <Robert.J.Moore@allanbank.com>
2014-05-13 00:01:53 -0400

    Explicit License.

--- mongodb/src/main/java/com/yahoo/ycsb/db/AsyncMongoDbClient.java
+++ mongodb/src/main/java/com/yahoo/ycsb/db/AsyncMongoDbClient.java
@@ -1,9 +1,19 @@
-/**
- * MongoDB client binding for YCSB using the Asynchronous Java Driver.
- *
- * Submitted by Rob Moore.
+/**                                                                                                                                                                                
+ * Copyright (c) 2013-2014, Allanbank Consulting, Inc. All rights reserved.                                                                                                                             
+ *                                                                                                                                                                                 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you                                                                                                             
+ * may not use this file except in compliance with the License. You                                                                                                                
+ * may obtain a copy of the License at                                                                                                                                             
+ *                                                                                                                                                                                 
+ * http://www.apache.org/licenses/LICENSE-2.0                                                                                                                                      
+ *                                                                                                                                                                                 
+ * Unless required by applicable law or agreed to in writing, software                                                                                                             
+ * distributed under the License is distributed on an "AS IS" BASIS,                                                                                                               
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or                                                                                                                 
+ * implied. See the License for the specific language governing                                                                                                                    
+ * permissions and limitations under the License. See accompanying                                                                                                                 
+ * LICENSE file.                                                                                                                                                                   
  */
-
 package com.yahoo.ycsb.db;
 
 import static com.allanbank.mongodb.builder.QueryBuilder.where;
@@ -46,7 +56,7 @@ import com.yahoo.ycsb.DBException;
  * mongodb.writeConcern=normal
  * 
  * @author rjm
- * 
+ * @copyright 2013-2014, Allanbank Consulting, Inc., All Rights Reserved
  */
 public class AsyncMongoDbClient extends DB {
 
