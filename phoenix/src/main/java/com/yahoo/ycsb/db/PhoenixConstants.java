2bcad91e2f6fd9c6041931fe9a423c0564201dc9
mujtabachohan <mujtabachohan@me.com>
2014-06-17 13:14:40 -0700

    Apache Phoenix http://phoenix.apache.org/ implementation for YCSB

---
+++ phoenix/src/main/java/com/yahoo/ycsb/db/PhoenixConstants.java
@@ -0,0 +1,13 @@
+package com.yahoo.ycsb.db;
+
+/**
+ * Phoenix constants
+ * 
+ * @author mujtaba
+ * 
+ */
+public interface PhoenixConstants {
+	  public static final String PRIMARY_KEY = "pkey";
+	  public static final int SUCCESS_CODE = 0;
+	  public static final int FAILURE_CODE = -1;
+}
