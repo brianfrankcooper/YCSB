d2e6af0bbeb26542a0f73a47931a6ef5354250b3
lyubent <ltodorov@dundee.ac.uk>
2014-07-15 02:32:02 +0300

    Part 11: fixes #2 - Update Aerospike client for Aerospike v3, and modify CRU & Scan method signatures

--- aerospike/src/main/java/com/yahoo/ycsb/db/AerospikeClient.java
+++ aerospike/src/main/java/com/yahoo/ycsb/db/AerospikeClient.java
@@ -2,53 +2,53 @@ package com.yahoo.ycsb.db;
 
 import java.util.*;
 
-import net.citrusleaf.CitrusleafClient;
-import net.citrusleaf.CitrusleafClient.ClResult;
-import net.citrusleaf.CitrusleafClient.ClResultCode;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+import java.util.Vector;
 
-import com.yahoo.ycsb.ByteArrayByteIterator;
+import com.aerospike.client.AerospikeException;
+import com.aerospike.client.Bin;
+import com.aerospike.client.Key;
+import com.aerospike.client.ResultCode;
+import com.aerospike.client.policy.Policy;
+import com.aerospike.client.policy.WritePolicy;
 import com.yahoo.ycsb.ByteIterator;
 import com.yahoo.ycsb.DBException;
 
 public class AerospikeClient extends com.yahoo.ycsb.DB{
 
-    public static final int OK = 0;
-    public static final int NULL_RESULT = -20;
-    /**
-     *  Mapping of ClResultCodes to documented Client Return Error codes
-     *  @see http://www.aerospike.com/quick-install/documentation/troubleshooting-guide/#clientcodes
-     */
-	private static final Map<ClResultCode, Integer> RESULT_CODE_MAPPER;
-    static {
-        RESULT_CODE_MAPPER = new EnumMap<ClResultCode, Integer>(ClResultCode.class);
+	public static final int OK = 0;
+	public static final int NULL_RESULT = -20;
+	private static final Map<Integer, Integer> RESULT_CODE_MAPPER;
+	static {
+		RESULT_CODE_MAPPER = new HashMap<Integer, Integer>();
+		RESULT_CODE_MAPPER.put(ResultCode.SERVER_ERROR, 1);
+		RESULT_CODE_MAPPER.put(ResultCode.KEY_NOT_FOUND_ERROR, 2);
+		RESULT_CODE_MAPPER.put(ResultCode.GENERATION_ERROR, 3);
+		RESULT_CODE_MAPPER.put(ResultCode.PARAMETER_ERROR, 4);
+		RESULT_CODE_MAPPER.put(ResultCode.KEY_EXISTS_ERROR, 5);
+		RESULT_CODE_MAPPER.put(ResultCode.BIN_EXISTS_ERROR, 6);
+		RESULT_CODE_MAPPER.put(ResultCode.CLUSTER_KEY_MISMATCH, 7);
+		RESULT_CODE_MAPPER.put(ResultCode.SERVER_MEM_ERROR, 8);
+		RESULT_CODE_MAPPER.put(ResultCode.TIMEOUT, 9);
+		RESULT_CODE_MAPPER.put(ResultCode.NO_XDS, 10);
+		RESULT_CODE_MAPPER.put(ResultCode.SERVER_NOT_AVAILABLE, 11);
+		RESULT_CODE_MAPPER.put(ResultCode.BIN_TYPE_ERROR, 12);
+		RESULT_CODE_MAPPER.put(ResultCode.RECORD_TOO_BIG, 13);
+		RESULT_CODE_MAPPER.put(ResultCode.KEY_BUSY, 14);
+		RESULT_CODE_MAPPER.put(ResultCode.OK, OK);
+		RESULT_CODE_MAPPER.put(ResultCode.SERIALIZE_ERROR, -10);
+	}
 
-        RESULT_CODE_MAPPER.put(ClResultCode.SERVER_ERROR, 1);
-        RESULT_CODE_MAPPER.put(ClResultCode.KEY_NOT_FOUND_ERROR, 2);
-        RESULT_CODE_MAPPER.put(ClResultCode.GENERATION_ERROR, 3);
-        RESULT_CODE_MAPPER.put(ClResultCode.PARAMETER_ERROR, 4);
-        RESULT_CODE_MAPPER.put(ClResultCode.KEY_EXISTS_ERROR, 5);
-        RESULT_CODE_MAPPER.put(ClResultCode.BIN_EXISTS_ERROR, 6);
-        RESULT_CODE_MAPPER.put(ClResultCode.CLUSTER_KEY_MISMATCH, 7);
-        RESULT_CODE_MAPPER.put(ClResultCode.SERVER_MEM_ERROR, 8);
-        RESULT_CODE_MAPPER.put(ClResultCode.TIMEOUT, 9);
-        RESULT_CODE_MAPPER.put(ClResultCode.NO_XDS, 10);
-        RESULT_CODE_MAPPER.put(ClResultCode.SERVER_NOT_AVAILABLE, 11);
-        RESULT_CODE_MAPPER.put(ClResultCode.BIN_TYPE_ERROR, 12);
-        RESULT_CODE_MAPPER.put(ClResultCode.RECORD_TOO_BIG, 13);
-        RESULT_CODE_MAPPER.put(ClResultCode.KEY_BUSY, 14);
-
-        RESULT_CODE_MAPPER.put(ClResultCode.OK, OK);
-        RESULT_CODE_MAPPER.put(ClResultCode.CLIENT_ERROR, -4);
-        RESULT_CODE_MAPPER.put(ClResultCode.SERIALIZE_ERROR, -10);
-
-        RESULT_CODE_MAPPER.put(ClResultCode.NOT_SET, -1);
-    }
-
-    private CitrusleafClient cl;
-
+	private com.aerospike.client.AerospikeClient as;
 	public static  String NAMESPACE = "test";
 
 	public static  String SET = "YCSB";
+
+	private Policy policy = new Policy();
+	private WritePolicy writePolicy = new WritePolicy();
 
 	public void init() throws DBException {
 		Properties props = getProperties();
@@ -81,94 +81,93 @@ public class AerospikeClient extends com.yahoo.ycsb.DB{
 			SET = st;
 		}
 
-		cl = new CitrusleafClient(host, port);
+		try{
+			as = new com.aerospike.client.AerospikeClient(host, port);
+		} catch (AerospikeException e){
+			throw new DBException(String.format("Failed to add %s:%d to cluster.", host, port));
+		}
+	}
+
+	public void cleanup() throws DBException {
+		as.close();
+	}
+
+	@Override
+	public int readOne(String table, String key, String field, Map<String,ByteIterator> result) {
 		try {
-			//Sleep so that the partition hashmap is created by the client
-			Thread.sleep(2000);
-		} catch (InterruptedException ex) {
-		}
-		
-		if (!cl.isConnected()) {
-			throw new DBException(String.format("Failed to add %s:%d to cluster.", 
-				host, port));
-		}
-
-	}
-
-		public void cleanup() throws DBException {
-		cl.close();
-	}
-
-    @Override
-	public int read(String table, String key, Set<String> fields,
-			HashMap<String, ByteIterator> result) {
-
-		if(fields != null) {
-			for (String bin : fields) {
-				ClResult res = cl.get(NAMESPACE, SET, key, bin, null);
-				if (res.result != null){
-					result.put(bin, new ByteArrayByteIterator(res.result.toString().getBytes()));
-				} else {
-					return NULL_RESULT;
-                }
-                if (res.resultCode != ClResultCode.OK) {
-                    return RESULT_CODE_MAPPER.get(res.resultCode);
-                }
-			}
-            return OK;
-		}
-		else {
-			ClResult res = cl.getAll(NAMESPACE, SET, key, null);
-			if (res.resultCode == ClResultCode.OK) {
-				for(Map.Entry<String, Object> entry : res.results.entrySet()) {
-					result.put(entry.getKey(), new ByteArrayByteIterator(entry.getValue().toString().getBytes()));
-				}
-			}
-			return RESULT_CODE_MAPPER.get(res.resultCode);
+			as.get(policy, new Key(NAMESPACE, SET, key), field);
+			return OK;
+		} catch (AerospikeException e) {
+			return RESULT_CODE_MAPPER.get(e.getResultCode());
 		}
 	}
 
 	@Override
-	public int scan(String table, String startkey, int recordcount,
-			Set<String> fields, Vector<HashMap<String, ByteIterator>> result) {
-		// TODO Auto-generated method stub
+	public int readAll(String table, String key, Map<String,ByteIterator> result) {
+		try {
+			as.get(policy, new Key(NAMESPACE, SET, key));
+			return OK;
+		} catch (AerospikeException e) {
+			return RESULT_CODE_MAPPER.get(e.getResultCode());
+		}
+	}
+
+	@Override
+	public int scanOne(String table, String startkey, int recordcount, String field, List<Map<String, ByteIterator>> result) {
+
+		System.out.println("Unsupported operation.");
 		return -1;
 	}
 
 	@Override
-	public int update(String table, String key,
-			HashMap<String, ByteIterator> values) {
-		Map<String, Object> v = new HashMap<String, Object>();
+	public int scanAll(String table, String startkey, int recordcount, List<Map<String, ByteIterator>> result) {
 
-		for (Map.Entry<String, ByteIterator> entry : values.entrySet()){
-			v.put(entry.getKey(), entry.getValue().toString());
-		}
-
-		ClResultCode rc = cl.set(NAMESPACE, SET, key, v, null, null);
-
-		return RESULT_CODE_MAPPER.get(rc);
+		System.out.println("Unsupported operation.");
+		return -1;
 	}
 
 	@Override
-	public int insert(String table, String key,
-			HashMap<String, ByteIterator> values) {
-		Map<String, Object> v = new HashMap<String, Object>();
+	public int updateOne(String table, String key, String field, ByteIterator value) {
 
-        for (Map.Entry<String, ByteIterator> entry : values.entrySet()){
-            v.put(entry.getKey(), entry.getValue().toString());
-        }
+		return update(key, Collections.singletonMap(field, value));
+	}
 
-        ClResultCode rc = cl.set(NAMESPACE, SET, key, v, null, null);
+	@Override
+	public int updateAll(String table, String key, Map<String,ByteIterator> values) {
 
-        return RESULT_CODE_MAPPER.get(rc);
+		return update(key, values);
+	}
+
+	public int update(String key, Map<String, ByteIterator> values) {
+
+		Bin[] bins = new Bin[values.size()];
+		int index = 0;
+		for (Map.Entry<String, ByteIterator> entry : values.entrySet() ) {
+			bins[index] = new Bin(entry.getKey(), entry.getValue().toArray() );
+			index++;
+		}
+		try {
+			as.put(writePolicy, new Key(NAMESPACE, SET, key), bins);
+			return OK;
+		} catch (AerospikeException e){
+			return e.getResultCode();
+		}
+	}
+
+	@Override
+	public int insert(String table, String key, Map<String, ByteIterator> values) {
+		return updateAll(table, key, values);
 	}
 
 	@Override
 	public int delete(String table, String key) {
-        ClResultCode rc = cl.delete(NAMESPACE, SET, key, null, null);
-        return RESULT_CODE_MAPPER.get(rc);
+		try {
+			as.delete(writePolicy, new Key(NAMESPACE, SET, key));
+			return OK;
+		} catch (AerospikeException e){
+			return RESULT_CODE_MAPPER.get(e.getResultCode());
+		}
 	}
-
 }
 
 
