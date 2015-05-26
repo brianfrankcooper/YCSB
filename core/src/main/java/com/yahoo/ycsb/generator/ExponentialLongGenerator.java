834b8eac47c588505faac0fd553f258f0b1ec968
twilli3141 <terry.l.williams@aexp.com>
2015-05-26 10:29:26 -0700

    Corrected year of authorship.

--- core/src/main/java/com/yahoo/ycsb/generator/ExponentialLongGenerator.java
+++ core/src/main/java/com/yahoo/ycsb/generator/ExponentialLongGenerator.java
@@ -1,5 +1,5 @@
 /**                                                                                                                                                                                
- * Copyright (c) 2011 Yahoo! Inc. All rights reserved.                                                                                                                             
+ * Copyright (c) 2013 Yahoo! Inc. All rights reserved.
  *                                                                                                                                                                                 
  * Licensed under the Apache License, Version 2.0 (the "License"); you                                                                                                             
  * may not use this file except in compliance with the License. You                                                                                                                
