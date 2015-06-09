5f87d095a6c644f6610adf4663e690cfcf44e3c8
Steffen Friedrich <friedrich@informatik.uni-hamburg.de>
2015-06-09 12:41:58 +0200

    [exporter] added JSONArrayMeasurementsExporter which generates one JSON array of measurement objects.

--- core/src/main/java/com/yahoo/ycsb/measurements/exporter/JSONMeasurementsExporter.java
+++ core/src/main/java/com/yahoo/ycsb/measurements/exporter/JSONMeasurementsExporter.java
@@ -1,12 +1,12 @@
-/**                                                                                                                                                                                
+/**
  * Copyright (c) 2010 Yahoo! Inc. All rights reserved.                                                                                                                             
- *                                                                                                                                                                                 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you                                                                                                             
  * may not use this file except in compliance with the License. You                                                                                                                
  * may obtain a copy of the License at                                                                                                                                             
- *                                                                                                                                                                                 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0                                                                                                                                      
- *                                                                                                                                                                                 
+ *
  * Unless required by applicable law or agreed to in writing, software                                                                                                             
  * distributed under the License is distributed on an "AS IS" BASIS,                                                                                                               
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or                                                                                                                 
@@ -16,14 +16,14 @@
  */
 package com.yahoo.ycsb.measurements.exporter;
 
-import org.codehaus.jackson.JsonFactory;
-import org.codehaus.jackson.JsonGenerator;
-import org.codehaus.jackson.util.DefaultPrettyPrinter;
-
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+
+import org.codehaus.jackson.JsonFactory;
+import org.codehaus.jackson.JsonGenerator;
+import org.codehaus.jackson.util.DefaultPrettyPrinter;
 
 /**
  * Export measurements into a machine readable JSON file.
