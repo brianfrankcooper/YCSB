c148fb50d0dbc6a0593cb9af81e361b5b41761e9
Sean Busbey <busbey@cloudera.com>
2015-06-18 02:42:34 -0500

    [infinispan] clean up refactoring of infinispan binding.
    * use double-check idiom for remote cache manager.
    * update version to 7.2.2.Final

--- infinispan/src/main/java/com/yahoo/ycsb/db/RemoteCacheManagerHolder.java
+++ infinispan/src/main/java/com/yahoo/ycsb/db/RemoteCacheManagerHolder.java
@@ -6,16 +6,20 @@ import org.infinispan.client.hotrod.RemoteCacheManager;
 
 public class RemoteCacheManagerHolder {
 	
-	private static RemoteCacheManager cacheManager = null;
+	private static volatile RemoteCacheManager cacheManager = null;
 	
 	private RemoteCacheManagerHolder() {}
 	
 	public static RemoteCacheManager getInstance(Properties props){
-		if(cacheManager == null){
-			synchronized (RemoteCacheManager.class) {
-				cacheManager = new RemoteCacheManager(props);
+		RemoteCacheManager result = cacheManager;
+		if(result == null){
+			synchronized (RemoteCacheManagerHolder.class) {
+				result = cacheManager;
+				if (result == null) {
+					cacheManager = result = new RemoteCacheManager(props);
+				}
 			}
 		}
-		return cacheManager;
+		return result;
 	}
 }
