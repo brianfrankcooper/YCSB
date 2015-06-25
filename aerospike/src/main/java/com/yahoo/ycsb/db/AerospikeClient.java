13e4086131ce15468e91c6e211827fc45be51749
Thomas Lopatic <thomas@lopatic.de>
2015-06-25 16:59:14 +0530

    Better error messages.

--- aerospike/src/main/java/com/yahoo/ycsb/db/AerospikeClient.java
+++ aerospike/src/main/java/com/yahoo/ycsb/db/AerospikeClient.java
@@ -77,7 +77,7 @@ public class AerospikeClient extends com.yahoo.ycsb.DB {
           new com.aerospike.client.AerospikeClient(clientPolicy, host, port);
     } catch (AerospikeException e) {
       throw new DBException(String.format("Error while creating Aerospike " +
-          "client for %s:%d.", host, port));
+          "client for %s:%d.", host, port), e);
     }
   }
 
@@ -150,7 +150,7 @@ public class AerospikeClient extends com.yahoo.ycsb.DB {
         return RESULT_OK;
       } catch (AerospikeException e) {
         if (e.getResultCode() != ResultCode.DEVICE_OVERLOAD) {
-          System.err.println("Error while updating key " + key + ": " + e);
+          System.err.println("Error while writing key " + key + ": " + e);
           return RESULT_ERROR;
         }
 
