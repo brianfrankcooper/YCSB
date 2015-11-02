f69e31003287bad18064214a61a6f617770c04f3
kruthar <kruthar@gmail.com>
2015-11-02 11:57:52 -0600

    [jdbc] added junit testing stubs for jdbc-binding

---
+++ jdbc/src/test/java/com/yahoo/ycsb/db/JdbcDBClientTest.java
@@ -0,0 +1,35 @@
+package com.yahoo.ycsb.db;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Created by kruthar on 11/2/15.
+ */
+public class JdbcDBClientTest {
+    @Test
+    public void insertTest() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void updateTest() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void readTest() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void deleteTest() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void scanTest() {
+        assertTrue(true);
+    }
+}
