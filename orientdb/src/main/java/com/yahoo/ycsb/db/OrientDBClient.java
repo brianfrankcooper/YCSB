b867c4691402d7bdd9f7481c8184328f63bcb4f6
Andy Kruth <kruthar@gmail.com>
2016-01-04 13:06:47 -0600

    [orientdb] incrementing license years

--- orientdb/src/main/java/com/yahoo/ycsb/db/OrientDBClient.java
+++ orientdb/src/main/java/com/yahoo/ycsb/db/OrientDBClient.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2012 - 2015 YCSB contributors. All rights reserved.
+ * Copyright (c) 2012 - 2016 YCSB contributors. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you
  * may not use this file except in compliance with the License. You
