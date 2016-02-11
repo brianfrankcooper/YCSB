480856531192ef66cac12cb0906771737b4a9ee3
Swapnil Bawaskar <sbawaskar@pivotal.io>
2016-02-11 10:25:07 -0800

    [geode] fixing copyright year and @author tag

--- geode/src/main/java/com/yahoo/ycsb/db/GeodeClient.java
+++ geode/src/main/java/com/yahoo/ycsb/db/GeodeClient.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2013 - 2014 YCSB Contributors. All rights reserved.
+ * Copyright (c) 2013 - 2016 YCSB Contributors. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you
  * may not use this file except in compliance with the License. You
@@ -61,8 +61,6 @@ import java.util.Vector;
  * region. A cache.xml defining "usertable" region can be placed in the
  * working directory to override these region definitions.</p>
  * 
- * @author Swapnil Bawaskar (sbawaska at vmware)
- *
  */
 public class GeodeClient extends DB {
 
