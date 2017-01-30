87d398f7a856e0b53d55ffec8d8d1eca2d960b5c
Kevin Risden <risdenk@users.noreply.github.com>
2017-01-30 17:02:26 -0500

    [core] Fix checkstyle for Core miscellaneous (#901)

--- core/src/main/java/com/yahoo/ycsb/DB.java
+++ core/src/main/java/com/yahoo/ycsb/DB.java
@@ -1,18 +1,18 @@
-/**                                                                                                                                                                                
- * Copyright (c) 2010 Yahoo! Inc. All rights reserved.                                                                                                                             
- *                                                                                                                                                                                 
- * Licensed under the Apache License, Version 2.0 (the "License"); you                                                                                                             
- * may not use this file except in compliance with the License. You                                                                                                                
- * may obtain a copy of the License at                                                                                                                                             
- *                                                                                                                                                                                 
- * http://www.apache.org/licenses/LICENSE-2.0                                                                                                                                      
- *                                                                                                                                                                                 
- * Unless required by applicable law or agreed to in writing, software                                                                                                             
- * distributed under the License is distributed on an "AS IS" BASIS,                                                                                                               
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or                                                                                                                 
- * implied. See the License for the specific language governing                                                                                                                    
- * permissions and limitations under the License. See accompanying                                                                                                                 
- * LICENSE file.                                                                                                                                                                   
+/**
+ * Copyright (c) 2010-2016 Yahoo! Inc., 2017 YCSB contributors All rights reserved.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You
+ * may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License. See accompanying
+ * LICENSE file.
  */
 
 package com.yahoo.ycsb;
@@ -28,10 +28,10 @@ import java.util.Vector;
  * This class should be constructed using a no-argument constructor, so we can
  * load it dynamically. Any argument-based initialization should be
  * done by init().
- * 
+ *
  * Note that YCSB does not make any use of the return codes returned by this class.
  * Instead, it keeps a count of the return values and presents them to the user.
- * 
+ *
  * The semantics of methods such as insert, update and delete vary from database
  * to database.  In particular, operations may or may not be durable once these
  * methods commit, and some systems may return 'success' regardless of whether
@@ -41,97 +41,94 @@ import java.util.Vector;
  * target application.  For the sake of comparison between experiments we also 
  * recommend you explain the semantics you chose when presenting performance results.
  */
-public abstract class DB
-{
-	/**
-	 * Properties for configuring this DB.
-	 */
-	Properties _p=new Properties();
+public abstract class DB {
+  /**
+   * Properties for configuring this DB.
+   */
+  private Properties properties = new Properties();
 
-	/**
-	 * Set the properties for this DB.
-	 */
-	public void setProperties(Properties p)
-	{
-		_p=p;
+  /**
+   * Set the properties for this DB.
+   */
+  public void setProperties(Properties p) {
+    properties = p;
 
-	}
+  }
 
-	/**
-	 * Get the set of properties for this DB.
-	 */
-	public Properties getProperties()
-	{
-		return _p; 
-	}
+  /**
+   * Get the set of properties for this DB.
+   */
+  public Properties getProperties() {
+    return properties;
+  }
 
-	/**
-	 * Initialize any state for this DB.
-	 * Called once per DB instance; there is one DB instance per client thread.
-	 */
-	public void init() throws DBException
-	{
-	}
+  /**
+   * Initialize any state for this DB.
+   * Called once per DB instance; there is one DB instance per client thread.
+   */
+  public void init() throws DBException {
+  }
 
-	/**
-	 * Cleanup any state for this DB.
-	 * Called once per DB instance; there is one DB instance per client thread.
-	 */
-	public void cleanup() throws DBException
-	{
-	}
+  /**
+   * Cleanup any state for this DB.
+   * Called once per DB instance; there is one DB instance per client thread.
+   */
+  public void cleanup() throws DBException {
+  }
 
-	/**
-	 * Read a record from the database. Each field/value pair from the result will be stored in a HashMap.
-	 *
-	 * @param table The name of the table
-	 * @param key The record key of the record to read.
-	 * @param fields The list of fields to read, or null for all of them
-	 * @param result A HashMap of field/value pairs for the result
-	 * @return The result of the operation.
-	 */
-	public abstract Status read(String table, String key, Set<String> fields, HashMap<String,ByteIterator> result);
+  /**
+   * Read a record from the database. Each field/value pair from the result will be stored in a HashMap.
+   *
+   * @param table The name of the table
+   * @param key The record key of the record to read.
+   * @param fields The list of fields to read, or null for all of them
+   * @param result A HashMap of field/value pairs for the result
+   * @return The result of the operation.
+   */
+  public abstract Status read(String table, String key, Set<String> fields, HashMap<String, ByteIterator> result);
 
-	/**
-	 * Perform a range scan for a set of records in the database. Each field/value pair from the result will be stored in a HashMap.
-	 *
-	 * @param table The name of the table
-	 * @param startkey The record key of the first record to read.
-	 * @param recordcount The number of records to read
-	 * @param fields The list of fields to read, or null for all of them
-	 * @param result A Vector of HashMaps, where each HashMap is a set field/value pairs for one record
-	 * @return The result of the operation.
-	 */
-	public abstract Status scan(String table, String startkey, int recordcount, Set<String> fields, Vector<HashMap<String,ByteIterator>> result);
-	
-	/**
-	 * Update a record in the database. Any field/value pairs in the specified values HashMap will be written into the record with the specified
-	 * record key, overwriting any existing values with the same field name.
-	 *
-	 * @param table The name of the table
-	 * @param key The record key of the record to write.
-	 * @param values A HashMap of field/value pairs to update in the record
-	 * @return The result of the operation.
-	 */
-	public abstract Status update(String table, String key, HashMap<String,ByteIterator> values);
+  /**
+   * Perform a range scan for a set of records in the database. Each field/value pair from the result will be stored
+   * in a HashMap.
+   *
+   * @param table The name of the table
+   * @param startkey The record key of the first record to read.
+   * @param recordcount The number of records to read
+   * @param fields The list of fields to read, or null for all of them
+   * @param result A Vector of HashMaps, where each HashMap is a set field/value pairs for one record
+   * @return The result of the operation.
+   */
+  public abstract Status scan(String table, String startkey, int recordcount, Set<String> fields,
+                              Vector<HashMap<String, ByteIterator>> result);
 
-	/**
-	 * Insert a record in the database. Any field/value pairs in the specified values HashMap will be written into the record with the specified
-	 * record key.
-	 *
-	 * @param table The name of the table
-	 * @param key The record key of the record to insert.
-	 * @param values A HashMap of field/value pairs to insert in the record
-	 * @return The result of the operation.
-	 */
-	public abstract Status insert(String table, String key, HashMap<String,ByteIterator> values);
+  /**
+   * Update a record in the database. Any field/value pairs in the specified values HashMap will be written into the
+   * record with the specified record key, overwriting any existing values with the same field name.
+   *
+   * @param table The name of the table
+   * @param key The record key of the record to write.
+   * @param values A HashMap of field/value pairs to update in the record
+   * @return The result of the operation.
+   */
+  public abstract Status update(String table, String key, HashMap<String, ByteIterator> values);
 
-	/**
-	 * Delete a record from the database. 
-	 *
-	 * @param table The name of the table
-	 * @param key The record key of the record to delete.
-	 * @return The result of the operation.
-	 */
-	public abstract Status delete(String table, String key);
+  /**
+   * Insert a record in the database. Any field/value pairs in the specified values HashMap will be written into the
+   * record with the specified record key.
+   *
+   * @param table The name of the table
+   * @param key The record key of the record to insert.
+   * @param values A HashMap of field/value pairs to insert in the record
+   * @return The result of the operation.
+   */
+  public abstract Status insert(String table, String key, HashMap<String, ByteIterator> values);
+
+  /**
+   * Delete a record from the database.
+   *
+   * @param table The name of the table
+   * @param key The record key of the record to delete.
+   * @return The result of the operation.
+   */
+  public abstract Status delete(String table, String key);
 }
--- core/src/main/java/com/yahoo/ycsb/TerminatorThread.java
+++ core/src/main/java/com/yahoo/ycsb/TerminatorThread.java
@@ -1,17 +1,17 @@
 /**
- * Copyright (c) 2011 Yahoo! Inc. All rights reserved.                                                                                                                             
- *                                                                                                                                                                                 
- * Licensed under the Apache License, Version 2.0 (the "License"); you                                                                                                             
- * may not use this file except in compliance with the License. You                                                                                                                
- * may obtain a copy of the License at                                                                                                                                             
- *                                                                                                                                                                                 
- * http://www.apache.org/licenses/LICENSE-2.0                                                                                                                                      
- *                                                                                                                                                                                 
- * Unless required by applicable law or agreed to in writing, software                                                                                                             
- * distributed under the License is distributed on an "AS IS" BASIS,                                                                                                               
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or                                                                                                                 
- * implied. See the License for the specific language governing                                                                                                                    
- * permissions and limitations under the License. See accompanying                                                                                                                 
+ * Copyright (c) 2010-2016 Yahoo! Inc., 2017 YCSB contributors All rights reserved.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You
+ * may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License. See accompanying
  * LICENSE file.
  */
 package com.yahoo.ycsb;
@@ -21,28 +21,26 @@ import java.util.Collection;
 /**
  * A thread that waits for the maximum specified time and then interrupts all the client
  * threads passed at initialization of this thread.
- * 
+ *
  * The maximum execution time passed is assumed to be in seconds.
- * 
- * @author sudipto
  *
  */
 public class TerminatorThread extends Thread {
-  
+
   private final Collection<? extends Thread> threads;
   private long maxExecutionTime;
   private Workload workload;
   private long waitTimeOutInMS;
-  
+
   public TerminatorThread(long maxExecutionTime, Collection<? extends Thread> threads,
-      Workload workload) {
+                          Workload workload) {
     this.maxExecutionTime = maxExecutionTime;
     this.threads = threads;
     this.workload = workload;
     waitTimeOutInMS = 2000;
     System.err.println("Maximum execution time specified as: " + maxExecutionTime + " secs");
   }
-  
+
   public void run() {
     try {
       Thread.sleep(maxExecutionTime * 1000);
