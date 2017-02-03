b60d2673692242642876920e60ff731ec4fadb2d
Kevin Risden <risdenk@users.noreply.github.com>
2017-02-03 10:11:00 -0500

    [core] Fix checkstyle for generator package (#915)

--- core/src/main/java/com/yahoo/ycsb/generator/SequentialGenerator.java
+++ core/src/main/java/com/yahoo/ycsb/generator/SequentialGenerator.java
@@ -1,12 +1,12 @@
 /**
- * Copyright (c) 2016 YCSB Contributors All rights reserved.
- *
+ * Copyright (c) 2016-2017 YCSB Contributors All rights reserved.
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License"); you
  * may not use this file except in compliance with the License. You
  * may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
@@ -23,8 +23,8 @@ import java.util.concurrent.atomic.AtomicInteger;
  * Generates a sequence of integers 0, 1, ...
  */
 public class SequentialGenerator extends NumberGenerator {
-  final AtomicInteger counter;
-  int _interval, _countstart;
+  protected final AtomicInteger counter;
+  protected int interval, countstart;
 
   /**
    * Create a counter that starts at countstart.
@@ -32,32 +32,32 @@ public class SequentialGenerator extends NumberGenerator {
   public SequentialGenerator(int countstart, int countend) {
     counter = new AtomicInteger();
     setLastValue(counter.get());
-    _countstart = countstart;
-    _interval = countend - countstart + 1;
+    this.countstart = countstart;
+    interval = countend - countstart + 1;
   }
-  
+
   /**
    * If the generator returns numeric (integer) values, return the next value as an int.
    * Default is to return -1, which is appropriate for generators that do not return numeric values.
    */
   public int nextInt() {
-    int ret = _countstart + counter.getAndIncrement() % _interval;
+    int ret = countstart + counter.getAndIncrement() % interval;
     setLastValue(ret);
     return ret;
   }
-  
+
   @Override
   public Number nextValue() {
-    int ret = _countstart + counter.getAndIncrement() % _interval;
+    int ret = countstart + counter.getAndIncrement() % interval;
     setLastValue(ret);
     return ret;
   }
-  
+
   @Override
   public Number lastValue() {
     return counter.get() + 1;
   }
-  
+
   @Override
   public double mean() {
     throw new UnsupportedOperationException("Can't compute mean of non-stationary distribution!");
--- core/src/main/java/com/yahoo/ycsb/generator/SkewedLatestGenerator.java
+++ core/src/main/java/com/yahoo/ycsb/generator/SkewedLatestGenerator.java
@@ -1,18 +1,18 @@
-/**                                                                                                                                                                                
- * Copyright (c) 2010 Yahoo! Inc. All rights reserved.                                                                                                                             
- *                                                                                                                                                                                 
- * Licensed under the Apache License, Version 2.0 (the "License"); you                                                                                                             
- * may not use this file except in compliance with the License. You                                                                                                                
- * may obtain a copy of the License at                                                                                                                                             
- *                                                                                                                                                                                 
- * http://www.apache.org/licenses/LICENSE-2.0                                                                                                                                      
- *                                                                                                                                                                                 
- * Unless required by applicable law or agreed to in writing, software                                                                                                             
- * distributed under the License is distributed on an "AS IS" BASIS,                                                                                                               
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or                                                                                                                 
- * implied. See the License for the specific language governing                                                                                                                    
- * permissions and limitations under the License. See accompanying                                                                                                                 
- * LICENSE file.                                                                                                                                                                   
+/**
+ * Copyright (c) 2010-2016 Yahoo! Inc., 2017 YCSB contributors. All rights reserved.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You
+ * may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License. See accompanying
+ * LICENSE file.
  */
 
 package com.yahoo.ycsb.generator;
@@ -20,43 +20,37 @@ package com.yahoo.ycsb.generator;
 /**
  * Generate a popularity distribution of items, skewed to favor recent items significantly more than older items.
  */
-public class SkewedLatestGenerator extends NumberGenerator
-{
-	private CounterGenerator _basis;
-	private final ZipfianGenerator _zipfian;
+public class SkewedLatestGenerator extends NumberGenerator {
+  private CounterGenerator basis;
+  private final ZipfianGenerator zipfian;
 
-	public SkewedLatestGenerator(CounterGenerator basis)
-	{
-		_basis=basis;
-		_zipfian=new ZipfianGenerator(_basis.lastValue());
-		nextValue();
-	}
+  public SkewedLatestGenerator(CounterGenerator basis) {
+    this.basis = basis;
+    zipfian = new ZipfianGenerator(this.basis.lastValue());
+    nextValue();
+  }
 
-	/**
-	 * Generate the next string in the distribution, skewed Zipfian favoring the items most recently returned by the basis generator.
-	 */
-	@Override
-  public Long nextValue()
-	{
-		long max=_basis.lastValue();
-		long next=max-_zipfian.nextLong(max);
-		setLastValue(next);
-		return next;
-	}
+  /**
+   * Generate the next string in the distribution, skewed Zipfian favoring the items most recently returned by
+   * the basis generator.
+   */
+  @Override
+  public Long nextValue() {
+    long max = basis.lastValue();
+    long next = max - zipfian.nextLong(max);
+    setLastValue(next);
+    return next;
+  }
 
-	public static void main(String[] args)
-	{
-		SkewedLatestGenerator gen=new SkewedLatestGenerator(new CounterGenerator(1000));
-		for (int i=0; i<Integer.parseInt(args[0]); i++)
-		{
-			System.out.println(gen.nextString());
-		}
+  public static void main(String[] args) {
+    SkewedLatestGenerator gen = new SkewedLatestGenerator(new CounterGenerator(1000));
+    for (int i = 0; i < Integer.parseInt(args[0]); i++) {
+      System.out.println(gen.nextString());
+    }
+  }
 
-	}
-
-	@Override
-	public double mean() {
-		throw new UnsupportedOperationException("Can't compute mean of non-stationary distribution!");
-	}
-
+  @Override
+  public double mean() {
+    throw new UnsupportedOperationException("Can't compute mean of non-stationary distribution!");
+  }
 }
