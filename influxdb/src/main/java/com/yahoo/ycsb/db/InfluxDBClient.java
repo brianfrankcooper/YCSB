1c4d54cddfee106d7538288d51ad44b9d73cb613
Clemens Lieb <vogel612@gmx.de>
2018-01-24 16:22:27 +0100

    [atsd] Add copyright declaration

--- influxdb/src/main/java/com/yahoo/ycsb/db/InfluxDBClient.java
+++ influxdb/src/main/java/com/yahoo/ycsb/db/InfluxDBClient.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015 - 2017 Andreas Bader All rights reserved.
+ * Copyright (c) 2015 - 2017 Andreas Bader, 2018 YCSB Contributors All rights reserved.
  * <p>
  * Licensed under the Apache License, Version 2.0 (the "License"); you
  * may not use this file except in compliance with the License. You
