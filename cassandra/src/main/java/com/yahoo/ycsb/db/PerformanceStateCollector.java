bce3c0330406bf4783fb63475a21068b7b9d7cec
Hannes Rabo <hannes.rabo@gmail.com>
2019-10-09 14:34:55 +0200

    Added test scripts for getting performance metrics

--- cassandra/src/main/java/com/yahoo/ycsb/db/PerformanceStateCollector.java
+++ cassandra/src/main/java/com/yahoo/ycsb/db/PerformanceStateCollector.java
@@ -6,6 +6,9 @@ import org.jolokia.client.request.*;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Collecting Cassandra performance
+ */
 public class PerformanceStateCollector {
 //  Post to https://jsonplaceholder.typicode.com/posts
   // Get from /users
@@ -17,16 +20,16 @@ public class PerformanceStateCollector {
     String basePath = String.format("%s:%s/jolokia", url, port);
     J4pClient j4pClient = new J4pClient(basePath);
 
-    J4pRequest req1 = new J4pExecRequest("java.lang:type=Memory","gc");
-    J4pRequest req2 = new J4pReadRequest("java.lang:type=Memory","HeapMemoryUsage");
+    J4pRequest req1 = new J4pExecRequest("java.lang:type=Memory", "gc");
+    J4pRequest req2 = new J4pReadRequest("java.lang:type=Memory", "HeapMemoryUsage");
 
-    J4pReadRequest req = new J4pReadRequest("java.lang:type=Memory","HeapMemoryUsage");
+    J4pReadRequest req = new J4pReadRequest("java.lang:type=Memory", "HeapMemoryUsage");
     J4pReadResponse resp = j4pClient.execute(req);
     System.out.println((String) resp.getValue());
 
     System.out.println("------------------------------");
 
-    List<J4pResponse<J4pRequest>> responses = j4pClient.execute(req1,req2);
+    List<J4pResponse<J4pRequest>> responses = j4pClient.execute(req1, req2);
     Map memVals = responses.get(1).getValue();
     System.out.println("Memory used: " + memVals.get("used"));
   }
