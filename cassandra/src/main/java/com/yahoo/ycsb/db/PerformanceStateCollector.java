08580bb884606645f102841dbb88463992e4680f
Hannes Rabo <hannes.rabo@gmail.com>
2019-11-01 13:45:57 +0100

    Updated the performance state collection

--- cassandra/src/main/java/com/yahoo/ycsb/db/PerformanceStateCollector.java
+++ cassandra/src/main/java/com/yahoo/ycsb/db/PerformanceStateCollector.java
@@ -1,8 +1,12 @@
 package com.yahoo.ycsb.db;
 
 import org.jolokia.client.J4pClient;
-import org.jolokia.client.request.*;
+import org.jolokia.client.request.J4pReadRequest;
+import org.jolokia.client.request.J4pRequest;
+import org.jolokia.client.request.J4pResponse;
 
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -10,33 +14,74 @@ import java.util.Map;
  * Collecting Cassandra performance.
  */
 public final class PerformanceStateCollector {
-  //  Post to https://jsonplaceholder.typicode.com/posts
-  // Get from /users
-
+  private static String port = "8778";
+  private static String url = "http://127.0.0.1";
   private PerformanceStateCollector() {
     // NOP
   }
 
-  private static String port = "8778";
-  private static String url = "http://localhost";
-
   //CHECKSTYLE:OFF
   public static void main(String[] args) throws Exception {
-  //CHECKSTYLE:ON
-    String basePath = String.format("%s:%s/jolokia", url, port);
+    String basePath = String.format("%s:%s/jolokia/", url, port);
     J4pClient j4pClient = new J4pClient(basePath);
 
-    J4pRequest req1 = new J4pReadRequest("java.lang:type=Memory", "gc");
-    J4pRequest req2 = new J4pReadRequest("java.lang:type=Memory", "HeapMemoryUsage");
+    List<J4pRequest> requestList = new LinkedList<>();
+    requestList.add(new J4pReadRequest("java.lang:type=Memory", "HeapMemoryUsage"));
+    requestList.add(new J4pReadRequest("org.apache.cassandra.metrics:type=ClientRequest,scope=Read,name=Latency"));
+    requestList.add(new J4pReadRequest("org.apache.cassandra.metrics:type=ClientRequest,scope=Read,name=Timeouts"));
+    requestList.add(new J4pReadRequest("org.apache.cassandra.metrics:type=ClientRequest,scope=Write,name=Latency"));
+    requestList.add(new J4pReadRequest("org.apache.cassandra.metrics:type=ClientRequest,scope=Write,name=Timeouts"));
+    requestList.add(new J4pReadRequest("org.apache.cassandra.metrics:type=CommitLog,name=PendingTasks"));
+    requestList.add(new J4pReadRequest("org.apache.cassandra.metrics:type=CommitLog,name=WaitingOnCommit"));
+    requestList.add(
+        new J4pReadRequest("org.apache.cassandra.metrics:keyspace=ycsb,name=ReadLatency,scope=usertable,type=Table")
+    );
+    requestList.add(
+        new J4pReadRequest("org.apache.cassandra.metrics:keyspace=ycsb,name=WriteLatency,scope=usertable,type=Table")
+    );
 
-//    J4pReadRequest req = new J4pReadRequest("java.lang:type=Memory", "HeapMemoryUsage");
-//    J4pReadResponse resp = j4pClient.execute(req);
-    // System.out.println(resp.getValue());
+    List<J4pResponse<J4pRequest>> responses = j4pClient.execute(requestList);
 
-    System.out.println("------------------------------");
+    // Memory related
+    System.out.println("Memory related:");
+    Iterator<J4pResponse<J4pRequest>> iterator = responses.iterator();
+    Map memVals = iterator.next().getValue();
+    System.out.println("Memory used: " + memVals.get("used") + " b");
 
-    List<J4pResponse<J4pRequest>> responses = j4pClient.execute(req1, req2);
-    Map memVals = responses.get(1).getValue();
-    System.out.println("Memory used: " + memVals.get("used"));
+    // Latency
+    System.out.println("\nRead");
+    Map readLatencyVals = iterator.next().getValue();
+    // We also have the list "RecentValues" which contains all the recent values here
+    System.out.println("Latency: One minute rate: " + readLatencyVals.get("OneMinuteRate") + " us");
+    System.out.println("Latency: Five minute rate: " + readLatencyVals.get("FiveMinuteRate") + " us");
+
+    readLatencyVals = iterator.next().getValue();
+    System.out.println("Timeouts: One minute rate: " + readLatencyVals.get("OneMinuteRate") + " e/s");
+    System.out.println("Timeouts: Five minute rate: " + readLatencyVals.get("FiveMinuteRate") + " e/s");
+
+    System.out.println("\nWrite");
+    Map writeLatencyVals = iterator.next().getValue();
+    System.out.println("Latency: One minute rate: " + writeLatencyVals.get("OneMinuteRate") + " us");
+    System.out.println("Latency: Five minute rate: " + writeLatencyVals.get("FiveMinuteRate") + " us");
+
+    writeLatencyVals = iterator.next().getValue();
+    System.out.println("Timeouts: One minute rate: " + writeLatencyVals.get("OneMinuteRate") + " e/s");
+    System.out.println("Timeouts: Five minute rate: " + writeLatencyVals.get("FiveMinuteRate") + " e/s");
+
+    System.out.println("Commit log");
+    Map commitLogVals = iterator.next().getValue();
+    System.out.println("Pending tasks: " + commitLogVals.get("Value"));
+    commitLogVals = iterator.next().getValue();
+    System.out.println("Waiting on commit: " + commitLogVals.get("OneMinuteRate"));
+
+    System.out.println("Table info");
+    Map tableVals = iterator.next().getValue();
+    System.out.println("Read: One minute rate: " + tableVals.get("OneMinuteRate") + " e/s");
+    System.out.println("Read: Five minute rate: " + tableVals.get("FiveMinuteRate") + " e/s");
+    tableVals = iterator.next().getValue();
+    System.out.println("One minute rate: " + tableVals.get("OneMinuteRate") + " e/s");
+    System.out.println("Five minute rate: " + tableVals.get("FiveMinuteRate") + " e/s");
+
   }
+  //CHECKSTYLE:ON
 }
