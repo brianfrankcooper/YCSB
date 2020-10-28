7ba8fe46f8a7e94fdb47d82f7c0c935ea9b292a5
dritter <daniel.ritter@sap.com>
2020-10-28 16:18:38 +0100

    docker not needed

--- orientdb/dockers/docker_daemond.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-[ "$1" = "-h" ]     && echo "docker_daemond.sh [-h|start|stop|status|prune]"
-[ "$1" = "start" ]  && sudo systemctl start  docker.service
-[ "$1" = "stop" ]   && sudo systemctl stop   docker.service
-[ "$1" = "status" ] && sudo systemctl status docker.service
-[ "$1" = "prune" ]  && sudo docker system prune -a --volumes
