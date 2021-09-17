89cbce39cb4836026ba160b7167b696be2f17009
U-GLOBAL\D037559 <daniel.ritter@sap.com>
2021-09-17 10:27:18 +0200

    updated copyright to 2021

--- orientdb/plots/tool.sh
+++ orientdb/plots/tool.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (c) 2012 - 2020 YCSB contributors. All rights reserved.
+# Copyright (c) 2012 - 2021 YCSB contributors. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you
 # may not use this file except in compliance with the License. You
--- orientdb/src/test/java/site/ycsb/db/OrientDBClientBatchTest.java
+++ orientdb/src/test/java/site/ycsb/db/OrientDBClientBatchTest.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2012 - 2020 YCSB contributors. All rights reserved.
+ * Copyright (c) 2012 - 2021 YCSB contributors. All rights reserved.
  *
  * <p>Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
  * except in compliance with the License. You may obtain a copy of the License at
